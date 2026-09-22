fde14397da0fcbaa3bde215e217bd633844d183c
Uriy Paramonov <arracondus@yandex.ru>
2026-09-22 04:02:12 +0300

    Add reporter test script

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+from src.reporter import DataFrameReporter
+
+
+def main():
+    data = pd.read_csv('data/payments.csv')
+
+    reporter = DataFrameReporter()
+    reporter.show_report(data)
+
+
+if __name__ == '__main__':
+    main()
